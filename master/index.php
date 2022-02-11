abd790d22f9abd9b3d8f882deef9389777a14509
iyadeh <94737123+iyadeh@users.noreply.github.com>
2022-02-11 21:46:00 +0800

    Menambahkan file index.php pada folder master
    folder master untuk admin side

---
+++ master/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en" style="height: 1073px;margin: 0px;">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alumni+Sans&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700&amp;display=swap">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css">
+    <link rel="stylesheet" href="../assets/css/styles.min.css?h=afd71d53af4af9deb59ab7881120ba9e">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.css">
+</head>
+
+<body class="d-flex d-xxl-flex justify-content-center align-items-center justify-content-xxl-center align-items-xxl-center" style="background: url(&quot;../assets/img/admin_bg.png?h=d48309209de4b47e32bdb528503ca658&quot;) center / cover no-repeat;height: 832px;">
+    <!-- Start: 2 Rows 1+1 Columns -->
+    <div class="container" style="border-radius: 18px;background: rgba(255,255,255,0.28);">
+        <div class="row">
+            <div class="col-md-12">
+                <!-- Start: Navigation with Button -->
+                <nav class="navbar navbar-light navbar-expand-lg navigation-clean-button" style="background: rgba(255,255,255,0);">
+                    <div class="container"><a class="navbar-brand" href="#">ADMIN</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                        <div class="collapse navbar-collapse" id="navcol-1">
+                            <ul class="navbar-nav me-auto">
+                                <li class="nav-item"><a class="nav-link" href="ujian.php">Ujian</a></li>
+                                <li class="nav-item"><a class="nav-link" href="peserta.html">Peserta</a></li>
+                            </ul><span class="navbar-text actions"> <a class="login" href="#"></a><a class="btn btn-light shadow action-button" role="button" href="logout.php" style="background: rgb(111,18,255);">Keluar</a></span>
+                        </div>
+                    </div>
+                </nav><!-- End: Navigation with Button -->
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12" style="height: 520px;">
+                <!-- Start: Simple Slider -->
+                <div class="simple-slider" style="border-radius: 21px;background: rgba(255,255,255,0);">
+                    <!-- Start: Slideshow -->
+                    <div class="swiper-container">
+                        <!-- Start: Slide Wrapper -->
+                        <div class="swiper-wrapper">
+                            <!-- Start: Slide -->
+                            <div class="shadow-lg swiper-slide" style="background: url(&quot;https://cdn.bootstrapstudio.io/placeholders/1400x800.png&quot;) center center / cover no-repeat;border-radius: 21px;"></div><!-- End: Slide -->
+                            <!-- Start: Slide -->
+                            <div class="shadow-lg swiper-slide" style="background: url(&quot;https://cdn.bootstrapstudio.io/placeholders/1400x800.png&quot;) center center / cover no-repeat;border-radius: 21px;"></div><!-- End: Slide -->
+                            <!-- Start: Slide -->
+                            <div class="shadow-lg swiper-slide" style="background: url(&quot;https://cdn.bootstrapstudio.io/placeholders/1400x800.png&quot;) center center / cover no-repeat;border-radius: 21px;"></div><!-- End: Slide -->
+                        </div><!-- End: Slide Wrapper -->
+                        <!-- Start: Pagination -->
+                        <div class="swiper-pagination"></div><!-- End: Pagination -->
+                        <!-- Start: Previous -->
+                        <div class="swiper-button-prev"></div><!-- End: Previous -->
+                        <!-- Start: Next -->
+                        <div class="swiper-button-next"></div><!-- End: Next -->
+                    </div><!-- End: Slideshow -->
+                </div><!-- End: Simple Slider -->
+            </div>
+        </div>
+    </div><!-- End: 2 Rows 1+1 Columns -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.js"></script>
+    <script src="../assets/js/script.min.js?h=334b48fb223084a2659075094c4d7507"></script>
+</body>
+
+</html>
