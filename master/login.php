ab812c2433b66ea9a6edb963738a3186d829326b
iyadeh <94737123+iyadeh@users.noreply.github.com>
2022-02-11 22:03:30 +0800

    menambahkan file login.php untuk admin

---
+++ master/login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alumni+Sans&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700&amp;display=swap">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css">
+    <link rel="stylesheet" href="../assets/css/styles.min.css?h=afd71d53af4af9deb59ab7881120ba9e">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.css">
+</head>
+
+<body class="d-flex d-xxl-flex justify-content-center align-items-center justify-content-xxl-center align-items-xxl-center" style="height: 1085px;padding-top: 0px;background: url(&quot;../assets/img/admin_bg.png?h=d48309209de4b47e32bdb528503ca658&quot;) center / cover no-repeat;border-radius: 10px;">
+    <!-- Start: 1 Row 2 Columns -->
+    <div class="container-fluid">
+        <div class="row g-0 d-xxl-flex justify-content-xxl-center" style="height: 578px;">
+            <!-- Start: Image -->
+            <div class="col-md-6 col-xxl-4" style="border-radius: 10px;border-top-left-radius: 12px;border-bottom-left-radius: 12px;background: url(&quot;../assets/img/Wijaya%20Kusuma_Hitam.png?h=26c6cedbce9a03e12a39a4d6163f97f6&quot;) center / contain no-repeat;">
+                <div></div>
+            </div><!-- End: Image -->
+            <div class="col-md-6 col-xxl-5" style="background: rgba(234,77,212,0);">
+                <section class="shadow-lg login-clean" style="background: rgba(111,18,255,0.23);border-top-right-radius: 12px;border-bottom-left-radius: 0px;border-bottom-right-radius: 12px;">
+                    <form class="shadow-lg" method="post" id="admin_login_form" style="background: rgba(81,85,89,0);">
+                        <h2 class="visually-hidden">Login Form</h2>
+                        <div class="illustration"><i class="icon-login" style="border-color: rgb(196,43,185);color: rgb(196,43,185);"></i></div>
+                        <div class="mb-3">
+                            <input class="form-control" id="admin_email_address" type="email" name="admin_email_address" placeholder="Email">
+                        </div>
+                        <div class="mb-3">
+                            <input class="form-control" id="admin_password" type="password" name="admin_password" placeholder="Password">
+                        </div>
+                        <div class="mb-3">
+                            <input type="hidden" name="page" value="login" />
+                            <input type="hidden" name="action" value="login" />
+                            <input type="submit" name="admin_login" id="admin_login" class="btn btn-primary d-block w-100" value="Login" style="background: rgb(196,43,185);" />
+                            <!-- <button class="btn btn-primary d-block w-100" name="admin_login" id="admin_login" value="Login" type="submit" style="background: rgb(196,43,185);">Log In</button> -->
+                        </div>
+                        <a class="forgot" href="register.php">Register</a>
+                    </form>
+                </section>
+            </div>
+        </div>
+    </div><!-- End: 1 Row 2 Columns -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.js"></script>
+    <script src="../assets/js/script.min.js?h=334b48fb223084a2659075094c4d7507"></script>
+</body>
+
+</html>
