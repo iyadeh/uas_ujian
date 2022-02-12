b1eeb7b6c5aa6691442343bf78cb7490ad21b6f6
iyadeh <94737123+iyadeh@users.noreply.github.com>
2022-02-12 18:42:35 +0800

    menambahkan file index.php untuk client side

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en" style="height: 1073px;margin: 0px;">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alumni+Sans&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700&amp;display=swap">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css?h=afd71d53af4af9deb59ab7881120ba9e">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.css">
+
+</head>
+
+<body class="d-flex d-xxl-flex justify-content-center align-items-center justify-content-xxl-center align-items-xxl-center" style="background: url(&quot;assets/img/peserta_bg.png?h=f7b1bbb9631751695e2d2b0d0bf58a9e&quot;) center / cover no-repeat;height: 832px;">
+    <!-- Start: 2 Rows 1+1 Columns -->
+    <div class="container" style="border-radius: 18px;background: rgba(255,255,255,0.28);">
+        <div class="row">
+            <div class="col-md-12">
+                <!-- Start: Navigation with Button -->
+                <nav class="navbar navbar-light navbar-expand-lg navigation-clean-button" style="background: rgba(255,255,255,0);">
+                    <div class="container"><a class="navbar-brand" href="#" style="color: #8f9ec3;">PESERTA</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                        <div class="collapse navbar-collapse" id="navcol-1">
+                            <ul class="navbar-nav me-auto">
+                                <li class="nav-item"><a class="nav-link" href="ujian.html" style="color: #8f9ec3;">Ujian</a></li>
+                                <li class="nav-item"><a class="nav-link" href="daftar_ujian.html" style="color: #8f9ec3;">Peserta</a></li>
+                            </ul><span class="navbar-text actions"> <a class="login" href="#"></a><a class="btn btn-light shadow action-button" role="button" href="index_admin.html" style="background: rgb(13,12,76);">Keluar</a></span>
+                        </div>
+                    </div>
+                </nav><!-- End: Navigation with Button -->
+            </div>
+        </div>
+        <div class="row d-xl-flex justify-content-xl-center align-items-xl-center" style="height: 220px;">
+            <div class="col-md-12 d-xl-flex justify-content-xl-center align-items-xl-center" style="height: 220px;"><select class="shadow-lg form-select-lg" style="border-radius: 16px;border-color: rgba(0,0,0,0);background: rgb(13,12,76);color: var(--bs-white);">
+                    <optgroup label="This is a group">
+                        <option value="1">apa</option>
+                        <option value="2">dimana</option>
+                    </optgroup>
+                </select></div>
+        </div>
+    </div><!-- End: 2 Rows 1+1 Columns -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.js"></script>
+    <script src="assets/js/script.min.js?h=334b48fb223084a2659075094c4d7507"></script>
+</body>
+
+</html>
