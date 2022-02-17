73fe289a11756041b13e4ef212d9e1f73696714c
iyadeh <94737123+iyadeh@users.noreply.github.com>
2022-02-18 07:52:28 +0800

    memperbaiki halaman utama peserta

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html>
-
 <?php
 
 $_POST['user']=true;
@@ -9,7 +7,8 @@ $exam->user_session_private();
  
 ?>
 
-<html lang="en" style="height: 1073px;margin: 0px;">
+<!DOCTYPE html>
+<html lang="en" style="height: 1073px;">
 
 <head>
     <meta charset="utf-8">
@@ -20,42 +19,56 @@ $exam->user_session_private();
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700&amp;display=swap">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css">
-    <link rel="stylesheet" href="assets/css/styles.min.css?h=afd71d53af4af9deb59ab7881120ba9e">
+    <link rel="stylesheet" href="assets/css/styles.min.css?h=f48f749d5f1033436d1965c971a43629">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.css">
-
+    <script src="assets/js/jquery-3.6.0.js"></script>
+    <script src="assets/js/parsley.js"></script>
+    <script src="user_script/index_script.js"></script>
 </head>
 
 <body class="d-flex d-xxl-flex justify-content-center align-items-center justify-content-xxl-center align-items-xxl-center" style="background: url(&quot;assets/img/peserta_bg.png?h=f7b1bbb9631751695e2d2b0d0bf58a9e&quot;) center / cover no-repeat;height: 832px;">
     <!-- Start: 2 Rows 1+1 Columns -->
-    <div class="container" style="border-radius: 18px;background: rgba(255,255,255,0.28);">
+    <div class="container" style="border-radius: 14px;background: rgba(255,255,255,0.25);height: 793.797px;">
         <div class="row">
             <div class="col-md-12">
                 <!-- Start: Navigation with Button -->
                 <nav class="navbar navbar-light navbar-expand-lg navigation-clean-button" style="background: rgba(255,255,255,0);">
-                    <div class="container"><a class="navbar-brand" href="#" style="color: #8f9ec3;">PESERTA</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                    <div class="container"><a class="navbar-brand" href="#" style="color: rgb(193,197,212);">DAFTAR UJIAN</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
                         <div class="collapse navbar-collapse" id="navcol-1">
                             <ul class="navbar-nav me-auto">
-                                <li class="nav-item"><a class="nav-link" href="ujian.html" style="color: #8f9ec3;">Ujian</a></li>
-                                <li class="nav-item"><a class="nav-link" href="daftar_ujian.html" style="color: #8f9ec3;">Peserta</a></li>
-                            </ul><span class="navbar-text actions"> <a class="login" href="#"></a><a class="btn btn-light shadow action-button" role="button" href="index_admin.html" style="background: rgb(13,12,76);">Keluar</a></span>
+                                <li class="nav-item"><a class="nav-link" href="ujianku.php" style="color: rgb(193,197,212);">Ujianku</a></li>
+                                <li class="nav-item"><a class="nav-link" href="profil.php" style="color: rgb(193,197,212);">Profil</a></li>
+                            </ul><span class="navbar-text actions"> <a class="login" href="#"></a><a class="btn btn-light shadow action-button" role="button" href="logout.php" style="background: rgb(11,12,58);">Keluar</a></span>
                         </div>
                     </div>
                 </nav><!-- End: Navigation with Button -->
             </div>
         </div>
-        <div class="row d-xl-flex justify-content-xl-center align-items-xl-center" style="height: 220px;">
-            <div class="col-md-12 d-xl-flex justify-content-xl-center align-items-xl-center" style="height: 220px;"><select class="shadow-lg form-select-lg" style="border-radius: 16px;border-color: rgba(0,0,0,0);background: rgb(13,12,76);color: var(--bs-white);">
-                    <optgroup label="This is a group">
-                        <option value="1">apa</option>
-                        <option value="2">dimana</option>
-                    </optgroup>
-                </select></div>
+        <div class="row d-lg-flex justify-content-lg-center align-items-lg-center" style="height: 587.797px;">
+            <div class="col-md-12 d-flex d-sm-flex d-xl-flex justify-content-center align-items-start justify-content-sm-center align-items-sm-start align-items-lg-start justify-content-xl-center align-items-xl-start align-items-xxl-start" style="height: 693.797px;">
+                <div class="card shadow-lg d-xxl-flex" style="margin-top: 53px;background: rgba(255,255,255,0.53);border-radius: 12px;">
+                    <div class="card-body">
+                        <h4 class="d-xxl-flex justify-content-xxl-center card-title">TANTANG DIRIMU DAN PILIH UJIANMU SEKARANG JUGA!!!</h4>
+                        <div class="d-flex d-xxl-flex justify-content-center justify-content-xxl-center">
+                            <select class="shadow-lg form-select-lg form-control input-lg" data-bss-hover-animate="flash" id="exam_list" style="width: 242px;height: 50px;border-radius: 17px;background: rgba(255,255,255,0.15);border-color: rgba(0,0,0,0);" name="exam_list">
+                                <option value="">Pilih Ujian</option>
+                                <?php
+                                echo $exam->Fill_exam_list();
+                                ?>
+                            </select>
+                        </div>
+                        <div class="d-flex d-xxl-flex justify-content-center align-items-start justify-content-xxl-center" style="margin-top: 14px;margin-bottom: 14px;">
+                        <span id="exam_details"></span>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div><!-- End: 2 Rows 1+1 Columns -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.js"></script>
-    <script src="assets/js/script.min.js?h=334b48fb223084a2659075094c4d7507"></script>
+    <script src="assets/js/script.min.js?h=d197b728335ff717d60c7a2eafac4697"></script>
 </body>
 
 </html>
