1f14ef1afbe6228a70b9ad6b12999172d35fa00f
iyadeh <94737123+iyadeh@users.noreply.github.com>
2022-02-18 07:47:33 +0800

    Menambahkan halaman peserta_ujian.php untuk menampilkan peserta untuk ujian tertentu

---
+++ master/peserta_ujian.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en" style="height: 1073px;">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alumni+Sans&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700&amp;display=swap">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css">
+    <link rel="stylesheet" href="../assets/css/styles.min.css?h=f48f749d5f1033436d1965c971a43629">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.css">
+</head>
+
+<body class="d-flex d-xxl-flex justify-content-center align-items-center justify-content-xxl-center align-items-xxl-center" style="background: url(&quot;../assets/img/1.png?h=d48309209de4b47e32bdb528503ca658&quot;) center / cover no-repeat;height: 832px;">
+    <!-- Start: 2 Rows 1+1 Columns -->
+    <div class="container" style="border-radius: 14px;background: rgba(255,255,255,0.25);">
+        <div class="row">
+            <div class="col-md-12">
+                <!-- Start: Navigation with Button -->
+                <nav class="navbar navbar-light navbar-expand-lg navigation-clean-button" style="background: rgba(255,255,255,0);">
+                    <div class="container"><a class="navbar-brand" href="#">UJIAN</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                        <div class="collapse navbar-collapse" id="navcol-1">
+                            <ul class="navbar-nav me-auto">
+                                <li class="nav-item"><a class="nav-link" href="admin.html">Admin</a></li>
+                                <li class="nav-item"><a class="nav-link" href="peserta.html">Peserta</a></li>
+                            </ul><span class="navbar-text actions"> <a class="login" href="#"></a><a class="btn btn-light shadow action-button" role="button" href="logout.php" style="background: rgb(111,18,255);">Keluar</a></span>
+                        </div>
+                    </div>
+                </nav><!-- End: Navigation with Button -->
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card shadow-lg" style="background: rgba(255,255,255,0.4);border-radius: 21px;margin: 35px;">
+                    <div class="card-header">
+                        <nav class="navbar navbar-light navbar-expand-md">
+                            <div class="container-fluid"><a class="navbar-brand" href="#">PESERTA UJIAN</a></div>
+                        </nav>
+                    </div>
+                    <div class="card-body d-flex">
+                        <div class="table-responsive" style="width: 1298px;">
+                            <table class="table table-striped table-borderless" id="enroll_table">
+                                <thead class="text-center">
+                                    <tr>
+                                        <th>Profil</th>
+                                        <th>Nama</th>
+                                        <th>Gender</th>
+                                        <th>Telp</th>
+                                        <th>Email Status</th>
+                                        <th>Hasil</th>
+                                    </tr>
+                                </thead>
+                                <tbody></tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- End: 2 Rows 1+1 Columns -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.js"></script>
+    <script src="../assets/js/script.min.js?h=d197b728335ff717d60c7a2eafac4697"></script>
+</body>
+
+</html>
